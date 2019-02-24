3d334048ce95ef9fc0ec56c0c06253d8dc39a141
kmariv <aurosa.123@gmail.com>
2019-02-24 16:25:50 -0600

    Add Form Parser to test in Postman

--- snackStore/snackStore/settings.py
+++ snackStore/snackStore/settings.py
@@ -96,7 +96,12 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-    )
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
 }
 
 # Password validation
